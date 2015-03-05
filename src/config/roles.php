06534fedb310894e464ca75b0c0c181b9a3b1f5c
Roman Bičan <romanbican@seznam.cz>
2015-03-05 20:16:10 +0000

    fix indentation

--- src/config/roles.php
+++ src/config/roles.php
@@ -3,41 +3,41 @@
 return [
 
     /*
-	|--------------------------------------------------------------------------
-	| Package Connection
-	|--------------------------------------------------------------------------
-	|
-	| You can set a different database connection for this package. It will set
-	| new connection for models Role and Permission. When this option is null,
-	| it will connect to the main database, which is set up in database.php
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Package Connection
+    |--------------------------------------------------------------------------
+    |
+    | You can set a different database connection for this package. It will set
+    | new connection for models Role and Permission. When this option is null,
+    | it will connect to the main database, which is set up in database.php
+    |
+    */
 
     'connection' => null,
 
     /*
-	|--------------------------------------------------------------------------
-	| Slug Separator
-	|--------------------------------------------------------------------------
-	|
-	| Here you can change the slug separator. This is very important in matter
-	| of magic method __call() and also a SlugableTrait. The default value
-	| is a dot.
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Slug Separator
+    |--------------------------------------------------------------------------
+    |
+    | Here you can change the slug separator. This is very important in matter
+    | of magic method __call() and also a SlugableTrait. The default value
+    | is a dot.
+    |
+    */
 
     'separator' => '.',
 
     /*
-	|--------------------------------------------------------------------------
-	| Roles, Permissions and Allowed "Pretend"
-	|--------------------------------------------------------------------------
-	|
-	| You can pretend or simulate package behavior no matter what is in your
-	| database. It is really useful when you are testing you application.
-	| Set up what will methods is(), can() and allowed() return.
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Roles, Permissions and Allowed "Pretend"
+    |--------------------------------------------------------------------------
+    |
+    | You can pretend or simulate package behavior no matter what is in your
+    | database. It is really useful when you are testing you application.
+    | Set up what will methods is(), can() and allowed() return.
+    |
+    */
 
     'pretend' => [
 
