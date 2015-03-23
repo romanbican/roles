1dd15f8c1003aec4c6b1d05a50ac765e33b60c44
Ryan Kent <hello@interphased.com>
2015-03-23 12:43:24 -0400

    add nullable to migrations

--- src/migrations/2015_01_15_105324_create_roles_table.php
+++ src/migrations/2015_01_15_105324_create_roles_table.php
@@ -17,7 +17,7 @@ class CreateRolesTable extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->string('slug')->unique();
-			$table->string('description');
+			$table->string('description')->nullable();
 			$table->integer('level')->default(1);
 			$table->timestamps();
 		});
