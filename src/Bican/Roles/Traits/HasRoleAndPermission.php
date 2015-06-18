a4e1f74dc1b651ff03036f980ec2d22268e8eb42
Jesse Szypulski <jesse@firetactic.com>
2015-06-18 19:32:08 -0400

    Update HasRoleAndPermission.php

--- src/Bican/Roles/Traits/HasRoleAndPermission.php
+++ src/Bican/Roles/Traits/HasRoleAndPermission.php
@@ -148,11 +148,13 @@ trait HasRoleAndPermission
     public function rolePermissions()
     {
         if (!$roles = $this->getRoles()->lists('id')->toArray()) { $roles = []; }
+        
+        $prefix = config('database.connections.' . config('database.default') . '.prefix');
 
-        return Permission::select([config('roles.prefix') . 'permissions.*', config('roles.prefix') . 'permission_role.created_at as pivot_created_at', config('roles.prefix') . 'permission_role.updated_at as pivot_updated_at'])
-                ->join(config('roles.prefix') . 'permission_role', config('roles.prefix') . 'permission_role.permission_id', '=', config('roles.prefix') . 'permissions.id')->join(config('roles.prefix') . 'roles', config('roles.prefix') . 'roles.id', '=', config('roles.prefix') . 'permission_role.role_id')
-                ->whereIn(config('roles.prefix') . 'roles.id', $roles) ->orWhere(config('roles.prefix') . 'roles.level', '<', $this->level())
-                ->groupBy(config('roles.prefix') . 'permissions.id');
+        return Permission::select([$prefix . 'permissions.*', $prefix . 'permission_role.created_at as pivot_created_at', $prefix . 'permission_role.updated_at as pivot_updated_at'])
+                ->join($prefix . 'permission_role', $prefix . 'permission_role.permission_id', '=', $prefix . 'permissions.id')->join($prefix . 'roles', $prefix . 'roles.id', '=', $prefix . 'permission_role.role_id')
+                ->whereIn($prefix . 'roles.id', $roles) ->orWhere($prefix . 'roles.level', '<', $this->level())
+                ->groupBy($prefix . 'permissions.id');
     }
 
     /**
