1bb2e9358028339585e89b6af1cfcb182b3ecfd1
Bias Tegaralaga <tegaralaga@arim-tech.com>
2017-09-12 11:46:32 +0700

    Change lists method to pluck as in https://laravel.com/docs/5.3/upgrade#upgrade-5.2.0

--- src/Bican/Roles/Traits/HasRoleAndPermission.php
+++ src/Bican/Roles/Traits/HasRoleAndPermission.php
@@ -166,7 +166,7 @@ trait HasRoleAndPermission
 
         return $permissionModel::select(['permissions.*', 'permission_role.created_at as pivot_created_at', 'permission_role.updated_at as pivot_updated_at'])
                 ->join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')->join('roles', 'roles.id', '=', 'permission_role.role_id')
-                ->whereIn('roles.id', $this->getRoles()->lists('id')->toArray()) ->orWhere('roles.level', '<', $this->level())
+                ->whereIn('roles.id', $this->getRoles()->pluck('id')->toArray()) ->orWhere('roles.level', '<', $this->level())
                 ->groupBy(['permissions.id', 'pivot_created_at', 'pivot_updated_at']);
     }
 
